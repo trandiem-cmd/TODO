86d28b14a2fece934307ec5527a5b85536c64bd4
Diem Tran <emma.trandiem@gmail.com>
2026-03-21 19:02:50 +0200

    Part 3/8 - Making HTTP calls

--- js/index.js
+++ js/index.js
@@ -1,16 +1,55 @@
+const BACKEND_ROOT_URL = 'http://localhost:3001';
 const list = document.querySelector("ul");
 const input = document.querySelector("input");
+input.disabled = true;
+
+const renderTask = (task) =>{
+    const li = document.createElement('li');
+    li.setAttribute("class","list-group-item");
+    li.innerHTML = task;
+    list.append(li);
+    
+};
+
+const saveTask = async(task) => {
+    try{
+        const json = JSON.stringify({description: task});
+        const response = await fetch(BACKEND_ROOT_URL + '/new',{
+            method: 'POST',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body: json
+        })
+        return response.json()
+    } catch (error) {
+        alert("Error saving task" + error.message)
+    }
+};
+
+const getTasks = async () =>{
+    try{
+    const respone = await fetch(BACKEND_ROOT_URL);
+    const json = await respone.json();
+    json.forEach(task=>renderTask(task.description));
+    input.disabled = false;
+    }catch{
+        alert("Error retrieving tasks" + error.message)
+    }
+}
 
 input.addEventListener("keypress",(event)=>{
     if(event.key==="Enter"){
         event.preventDefault();
         const task = input.value.trim();
         if(task !== ''){
-            const li = document.createElement('li');
-            li.setAttribute("class","list-group-item");
-            li.innerHTML = task;
-            list.append(li);
-            input.value = "";
+            saveTask(task).then((json)=>{
+                renderTask(task);
+                 input.value = "";
+            })
         }
     }
-})
+});
+
+
+getTasks();
