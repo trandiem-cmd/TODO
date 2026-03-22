63263af5ac535a4902b4daea16173a28ae1aa69b
Diem Tran <emma.trandiem@gmail.com>
2026-03-22 11:00:12 +0200

    Part 4/8 - Creating classes

---
+++ js/class/Task.js
@@ -0,0 +1,16 @@
+class Task{
+    #id
+    #text
+    constructor(id,text){
+        this.#id = id,
+        this.#text = text
+    }
+    getId(){
+        return this.#id;
+    }
+    getText(){
+        return this.#text
+    }
+}
+
+export { Task };
--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 const BACKEND_ROOT_URL = 'http://localhost:3001';
+import { Todos } from './class/Todos.js';
+const todos = new Todos(BACKEND_ROOT_URL);
+
 const list = document.querySelector("ul");
 const input = document.querySelector("input");
 input.disabled = true;
@@ -6,36 +9,20 @@ input.disabled = true;
 const renderTask = (task) =>{
     const li = document.createElement('li');
     li.setAttribute("class","list-group-item");
-    li.innerHTML = task;
+    li.innerHTML = task.getText();
     list.append(li);
     
 };
 
-const saveTask = async(task) => {
-    try{
-        const json = JSON.stringify({description: task});
-        const response = await fetch(BACKEND_ROOT_URL + '/new',{
-            method: 'POST',
-            headers: {
-                'Content-Type':'application/json'
-            },
-            body: json
+const getTasks = () =>{
+    todos.getTasks().then((tasks)=>{
+        tasks.forEach(task=>{
+            renderTask(task)
         })
-        return response.json()
-    } catch (error) {
-        alert("Error saving task" + error.message)
-    }
-};
-
-const getTasks = async () =>{
-    try{
-    const respone = await fetch(BACKEND_ROOT_URL);
-    const json = await respone.json();
-    json.forEach(task=>renderTask(task.description));
-    input.disabled = false;
-    }catch{
-        alert("Error retrieving tasks" + error.message)
-    }
+        input.disabled = false
+    }).catch((error) => {
+        alert(error)
+    })
 }
 
 input.addEventListener("keypress",(event)=>{
@@ -43,9 +30,10 @@ input.addEventListener("keypress",(event)=>{
         event.preventDefault();
         const task = input.value.trim();
         if(task !== ''){
-            saveTask(task).then((json)=>{
+            todos.addTask(task).then((task)=>{
                 renderTask(task);
-                 input.value = "";
+                input.value = "";
+                input.focus();
             })
         }
     }
